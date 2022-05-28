0d3d06f71b9ecbef7174ef045d94fb23ec35b125
Yakhya Dabo <yakhyadabo@gmail.com>
2022-05-28 14:23:16 +0100

    [module] subnets data

---
+++ module/subnets.tf
@@ -0,0 +1,43 @@
+# VPC of the subnets
+data "aws_vpc" "main" {
+  tags = {
+    Name = var.vpc_name
+    Environment = var.environment
+  }
+}
+
+# Subnets of the EKS cluster
+data "aws_subnets" "eks_cluster" {
+  filter {
+    name   = local.vpc_id
+    values = [data.aws_vpc.main.id]
+  }
+
+  tags = {
+    Zone = local.zone.eks_cluster
+  }
+}
+
+# Private Subnets of the node groups
+data "aws_subnets" "private" {
+  filter {
+    name   = local.vpc_id
+    values = [data.aws_vpc.main.id]
+  }
+
+  tags = {
+    Zone = local.zone.private
+  }
+}
+
+# Public Subnets of the node groups
+data "aws_subnets" "public" {
+  filter {
+    name   = local.vpc_id
+    values = [data.aws_vpc.main.id]
+  }
+
+  tags = {
+    Zone = local.zone.public
+  }
+}
