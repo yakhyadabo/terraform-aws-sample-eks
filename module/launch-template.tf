0e0952585661b24ae757ad807e7bdb595d312231
Yakhya Dabo <yakhyadabo@gmail.com>
2023-01-27 16:15:36 +0000

    [module] Remove comments from launch_template

--- module/launch-template.tf
+++ module/launch-template.tf
@@ -1,9 +1,7 @@
 resource "aws_launch_template" "default" {
   name_prefix     = join("-", [var.service_name, var.environment])
   vpc_security_group_ids = [aws_security_group.ingress.id, aws_security_group.eks_cluster.id, aws_security_group.eks_nodes.id]
-
- // instance_type = "t3.medium"
-
+  
   block_device_mappings {
     device_name = "/dev/xvda"
 
@@ -15,32 +13,11 @@ resource "aws_launch_template" "default" {
 
   // See : https://docs.aws.amazon.com/eks/latest/userguide/launch-templates.html
 
-/*  user_data = base64encode(<<-EOF
-MIME-Version: 1.0
-Content-Type: multipart/mixed; boundary="==MYBOUNDARY=="
-
---==MYBOUNDARY==
-Content-Type: text/x-shellscript; charset="us-ascii"
-
-#!/bin/bash
-set -ex
-/etc/eks/bootstrap.sh my-cluster \
-  --b64-cluster-ca certificate-authority \
-  --apiserver-endpoint api-server-endpoint \
-  --dns-cluster-ip service-cidr.10 \
-  --container-runtime containerd \
-  --kubelet-extra-args '--max-pods=my-max-pods-value' \
-  --use-max-pods false
---==MYBOUNDARY==--\
-
-  EOF
-  )*/
-
   tags = {
     "eks:cluster-name"   = aws_eks_cluster.main.name
     "eks:nodegroup-name" = join("-", [var.service_name, var.environment,"node-group"])
   }
-  
+
   tag_specifications {
     resource_type = "instance"
 
