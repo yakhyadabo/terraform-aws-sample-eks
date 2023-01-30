2090cc98b1ba773cb17d576e1f7c127192628dfb
Yakhya Dabo <yakhyadabo@gmail.com>
2023-01-30 23:42:32 +0000

    [module] EKS cluster subnet ids

--- module/eks-cluster.tf
+++ module/eks-cluster.tf
@@ -6,7 +6,8 @@ resource "aws_eks_cluster" "main" {
     security_group_ids      = [aws_security_group.eks_cluster.id, aws_security_group.eks_nodes.id]
     endpoint_private_access = var.endpoint_private_access
     endpoint_public_access  = var.endpoint_public_access
-    subnet_ids = data.aws_subnets.eks_cluster.ids
+    // public_access_cidrs = ["0.0.0.0/0",]
+    subnet_ids = flatten([data.aws_subnets.private.ids, data.aws_subnets.public.ids ])
   }
 
   # Ensure that IAM Role permissions are created before and deleted after EKS Cluster handling.
@@ -14,4 +15,8 @@ resource "aws_eks_cluster" "main" {
   depends_on = [
     aws_iam_role_policy_attachment.aws_eks_cluster_policy
   ]
+
+  tags = {
+    Name = join("-", [var.service_name, var.environment])
+  }
 }
