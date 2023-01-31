cf93435701b6430e321ccc3e769422c49bea3912
Yakhya Dabo <yakhyadabo@gmail.com>
2023-01-31 14:59:51 +0000

    [instance] Add missing variables

--- dev.tf
+++ dev.tf
@@ -1,10 +1,16 @@
+locals {
+  cluster_name = format("%s-%s", var.project.name, var.project.environment)
+}
+
 module "eks" {
   source               = "./module"
   region               = var.region
-  environment          = var.project.environment
-  vpc_name             = "${var.project.environment}-vpc"
-  service_name         = var.service_name
+  cluster_name         = local.cluster_name
+  project_name         = var.project.name
+  vpc_name             = format("%s-vpc",var.project.environment)
   key_name             = var.key_name
-  cluster_sg_name      = "eks-cluster-sg"
-  nodes_sg_name = "node-group-sg"
+  cluster_sg_name      = format("%s-%s",local.cluster_name,"cluster-sg")
+  nodes_sg_name        = format("%s-%s",local.cluster_name,"node-group-sg")
+  private_subnets_cidr = var.private_subnets_cidr
+  public_subnets_cidr  = var.public_subnets_cidr
 }
