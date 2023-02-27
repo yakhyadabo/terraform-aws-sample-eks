60265e9537f24206b4e2538c815e59dd7cf52f37
Yakhya Dabo <yakhyadabo@gmail.com>
2023-02-27 15:10:59 +0000

    [dev] Refactor params

--- dev.tf
+++ dev.tf
@@ -1,5 +1,8 @@
 locals {
   cluster_name = format("%s-%s", var.project.name, var.project.environment)
+  vpc_name = format("%s-vpc",var.project.environment)
+  cluster_sg_name = format("%s-%s",local.cluster_name,"cluster-sg")
+  node_sg_name = format("%s-%s",local.cluster_name,"node-group-sg")
 }
 
 module "eks" {
@@ -7,10 +10,10 @@ module "eks" {
   region               = var.region
   cluster_name         = local.cluster_name
   project_name         = var.project.name
-  vpc_name             = format("%s-vpc",var.project.environment)
+  vpc_name             = local.vpc_name
   key_name             = var.key_name
-  cluster_sg_name      = format("%s-%s",local.cluster_name,"cluster-sg")
-  nodes_sg_name        = format("%s-%s",local.cluster_name,"node-group-sg")
+  cluster_sg_name      = local.cluster_name
+  nodes_sg_name        = local.node_sg_name
   private_subnets_cidr = var.private_subnets_cidr
   public_subnets_cidr  = var.public_subnets_cidr
 }
